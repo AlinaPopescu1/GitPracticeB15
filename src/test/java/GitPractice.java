a0e64a31f13be77c22cfa56e86db0912b4a98fef
AlinaPopescu1 <Alina.popescu.1993@gmail.com>
2020-04-16 17:16:05 -0400

    second commit

--- src/test/java/GitPractice.java
+++ src/test/java/GitPractice.java
@@ -2,6 +2,8 @@ public class GitPractice {
     public static void main(String[] args) {
         System.out.println("PRACTICE");
         System.out.println("PRACTICE");
+        
+        System.out.println("PRACTICE");
         System.out.println("PRACTICE");
 
     }
