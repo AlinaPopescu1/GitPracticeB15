2da65eccc8abb75064bcff24131c45f641bc67b8
AlinaPopescu1 <Alina.popescu.1993@gmail.com>
2020-04-17 14:05:43 -0400

    alina

--- src/test/java/GitPractice.java
+++ src/test/java/GitPractice.java
@@ -2,7 +2,7 @@ public class GitPractice {
     public static void main(String[] args) {
         System.out.println("PRACTICE");
         System.out.println("PRACTICE");
-        
+        System.out.println("Extra line");
         System.out.println("PRACTICE");
         System.out.println("PRACTICE");
 
